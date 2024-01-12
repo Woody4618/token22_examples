f55da984ac79d15bbe5f7d613a802ba8674c6a9e
Jonas Hahn <herzock@gmail.com>
2024-01-12 17:30:43 +0100

    SimpleTransferHook example

---
+++ NativeMetaData/createMint.ts
@@ -0,0 +1,10 @@
+
+// 
+
+spl-token --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb create-token --enable-metadata
+spl-token --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb initialize-metadata EBF22st7gRqvKtEYW2WomsHUsNrjtPcZvpDAnh6W7Ugu Snail SLUG https://github.com/solana-developers/solana-game-examples
+spl-token create-account EBF22st7gRqvKtEYW2WomsHUsNrjtPcZvpDAnh6W7Ugu
+spl-token --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb mint EBF22st7gRqvKtEYW2WomsHUsNrjtPcZvpDAnh6W7Ugu 100
+spl-token --program-id TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb update-metadata EBF22st7gRqvKtEYW2WomsHUsNrjtPcZvpDAnh6W7Ugu Snail SLUG https://shdw-drive.genesysgo.net/AzjHvXgqUJortnr5fXDG2aPkp2PfFMvu4Egr57fdiite/PirateCoinMeta
+
+SnailToken: EBF22st7gRqvKtEYW2WomsHUsNrjtPcZvpDAnh6W7Ugu
